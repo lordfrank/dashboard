f1ae1c975e38e9e7b9d4cbdf554ac7f699211ead
frank <frankcayod@gmail.com>
2017-12-12 17:56:01 -0300

    fix 2
    para os

--- get/eventos.php
+++ get/eventos.php
@@ -15,7 +15,7 @@ $sql="SELECT id_servicio,servicio_eventos.id,evento_id, max(estado_id) estado_id
 	$sql="SELECT y.*,(select servicio from servicios where id=id_servicio) nombre_servicio,if (y.evento_id=y.id,'Nuevo evento',(select concat(fecha,'- ',estado_evento,' ', detalle) from servicio_eventos x where x.id=y.evento_id)) nombre_evento,date_format(fecha,'%H:%i') hora,date_format(fecha,'%Y-%m-%d') fechaformato   FROM `servicio_eventos` y,estados where estado_id=estados.id  and y.id=$id";
 }else{
 	
-$sql="SELECT id_servicio,servicio_eventos.id,evento_id, max(estado_id) estado_id, min(fecha) fecha,max(fecha) fechafin,color,date(min(fecha)) original,servicio_eventos.estado  FROM `servicio_eventos`,estados where estado_id=estados.id and fecha>'".$fechai."' group by evento_id";
+$sql="SELECT id_servicio,servicio_eventos.id,evento_id, max(estado_id) estado_id, min(fecha) fecha,max(fecha) fechafin,color,date(min(fecha)) original,servicio_eventos.estado  FROM `servicio_eventos`,estados where estado_id=estados.id and fecha>'".$fechai."' group by id_servicio,evento_id,servicio_eventos.id,color,servicio_eventos.estado";
 }
 
 		$result=$mysqli->query($sql);
